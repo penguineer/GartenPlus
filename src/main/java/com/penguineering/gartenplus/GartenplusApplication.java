d034c5b9c80d1b6940c380a09fd37caf07270bbc
Stefan Haun <mail@tuxathome.de>
2024-07-17 19:34:51 +0200

    Add SpringBoot base application

---
+++ src/main/java/com/penguineering/gartenplus/GartenplusApplication.java
@@ -0,0 +1,13 @@
+package com.penguineering.gartenplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GartenplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GartenplusApplication.class, args);
+    }
+
+}
