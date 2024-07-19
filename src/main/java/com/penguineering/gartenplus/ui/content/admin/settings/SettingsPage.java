e71dca2cf030819f6fe0637fa7eff3451476f110
Stefan Haun <mail@tuxathome.de>
2024-07-19 23:02:13 +0200

    Add a generic settings page that forwards to the first tab

---
+++ src/main/java/com/penguineering/gartenplus/ui/content/admin/settings/SettingsPage.java
@@ -0,0 +1,18 @@
+package com.penguineering.gartenplus.ui.content.admin.settings;
+
+import com.penguineering.gartenplus.ui.appframe.GartenplusPage;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.PermitAll;
+
+@Route(value = "", layout = SettingsLayout.class)
+@PermitAll
+@PageTitle("GartenPlus | Einstellungen")
+public class SettingsPage extends GartenplusPage implements BeforeEnterObserver {
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        event.getUI().navigate(event.getLocation().getPath() + "/groups");
+    }
+}
