4edff3cfe5253e5f75319eb4fde0673ae76c4fe3
Stefan Haun <mail@tuxathome.de>
2024-07-19 17:47:45 +0200

    Set the custom login view for OIDC logins

--- src/main/java/com/penguineering/gartenplus/auth/SecurityConfig.java
+++ src/main/java/com/penguineering/gartenplus/auth/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.penguineering.gartenplus.auth;
 
+import com.penguineering.gartenplus.ui.content.LoginPage;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +12,10 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig extends VaadinWebSecurity {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        setLoginView(http, LoginPage.class);
+
         http.oauth2Login(login -> login
+                .loginPage("/login")
                 .defaultSuccessUrl("/"));
 
         http.sessionManagement(session -> session
@@ -24,6 +28,7 @@ public class SecurityConfig extends VaadinWebSecurity {
 
         // Allow unauthenticated access to Health Endpoint and logo
         http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/login")).permitAll()
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/assets/github-mark-white.svg")).permitAll()
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/actuator/health")).permitAll()
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/assets/GartenPlusLogo.png")).permitAll()
